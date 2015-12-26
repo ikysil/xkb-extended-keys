350246e719ba5e139f6d78a000c20c3fc7ba0ed4
Yoni Weill <jweill@google.com>
2015-12-26 23:48:55 +0200

    More change.

--- set_xkb_settings.sh
+++ set_xkb_settings.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 OUTPUT_DIR="$SCRIPT_DIR/build"
+GEN_CONFIG="$OUTPUT_DIR/xkbcustom_generated"
 BACKUP_FILE="$OUTPUT_DIR/backup"
+
+if [[ $# -eq 0 ]]; then
+  LAYOUT=us
+else
+  LAYOUT="$1"
+fi
 
 [ -d "$OUTPUT_DIR" ] || mkdir "$OUTPUT_DIR"
 
-# Create backup inside this folder
+# Create backup
 setxkbmap -print > "$BACKUP_FILE"
 
+# Create xkb file for the given layout
+cat "$SCRIPT_DIR/keymap/xkbcustom" | sed "s/LAYOUT/$LAYOUT/" > "$GEN_CONFIG"
+
 # Compile the custom XKB configuration
-xkbcomp -I"$SCRIPT_DIR" "$SCRIPT_DIR/keymap/xkbcustom" $DISPLAY
+xkbcomp -I"$SCRIPT_DIR" "$GEN_CONFIG" $DISPLAY
